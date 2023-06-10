67ecca5c60c288ec156a9a410e152e41665aaacf
FomichAnastasia <114520431+FomichAnastasia@users.noreply.github.com>
2023-06-10 10:21:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def prediction():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5444,host='0.0.0.0')
+    app.run(debug=True,port=5444,host='0.0.0.0') #port specification with port = ...
